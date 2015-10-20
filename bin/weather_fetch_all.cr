c87cffbec78c68f77e4179199fe39d4e5c6c4c40
Aleksander Kwiatkowski <bobikx@poczta.fm>
2015-10-20 14:52:27 +0200

    probably fix

--- bin/weather_fetch_all.cr
+++ bin/weather_fetch_all.cr
@@ -1,26 +1,5 @@
 require "../src/weather_crystal"
-require "yaml"
-require "logger"
 
 path = File.join("config", "weather.yml")
-cities = WeatherCrystal::WeatherCity.load_yaml(path)
-logger = Logger.new(STDOUT)
-sleep_amount = 60*10
-sleep_amount = 10
-
-loop do
-  cities.each do |city|
-
-    o = WeatherCrystal::Provider::Noaa.new( city )
-    weathers = o.fetch
-
-    storage = WeatherCrystal::WeatherStorage.new
-    count = storage.store( weathers )
-
-    logger.info("#{city.metar} done with #{count} weather data") if count > 0
-  end
-
-  logger.info("Sleep #{sleep_amount} seconds")
-  sleep sleep_amount
-
-end
+f = WeatherCrystal::WeatherFetcher.new(path)
+f.loop_fetch
--- src/weather_crystal/weather_fetcher.cr
+++ src/weather_crystal/weather_fetcher.cr
@@ -1,9 +1,35 @@
+require "yaml"
+require "logger"
+
 class WeatherCrystal::WeatherFetcher
-  def initialize
+  def initialize(config_path)
+    @cities = WeatherCrystal::WeatherCity.load_yaml(config_path)
+    @logger = Logger.new(STDOUT)
+    @sleep_amount = 60*10
+    @sleep_amount = 10
   end
 
-  property :metar
+  property :sleep_amount
 
-  def get
+  def one_fetch
+    @cities.each do |city|
+
+      o = WeatherCrystal::Provider::Noaa.new( city )
+      weathers = o.fetch
+
+      storage = WeatherCrystal::WeatherStorage.new
+      count = storage.store( weathers )
+
+      @logger.info("#{city.metar} done with #{count} weather data") if count > 0
+    end
+  end
+
+  def loop_fetch
+    while true
+      one_fetch
+
+      @logger.info("Sleep #{@sleep_amount} seconds")
+      sleep sleep_amount
+    end
   end
 end
